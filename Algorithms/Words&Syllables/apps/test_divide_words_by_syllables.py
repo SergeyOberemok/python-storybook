f337c48fc9fed609bfa4efd274843e0624133982
SergeyOberemok <sergey.oberemok@gmail.com>
2025-03-03 15:11:28 +0100

    [update]

--- Algorithms/Words&Syllables/apps/test_divide_words_by_syllables.py
+++
@@ -1,8 +0,0 @@
-from divide_words_by_syllables import divideWordsBySyllables
-
-
-def test_words_equality():
-    dividedWords: list[tuple[str, str]] = divideWordsBySyllables()
-
-    for (word, divided) in dividedWords:
-        assert word == divided.replace('-', ''), f'{word} != {divided}'
